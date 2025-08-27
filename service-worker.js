0ae90e487322f4139980bd6937a7e96cda1dfd5d
DevByRed <rayanelhejjami86@gmail.com>
2025-08-27 22:13:19 +0200

    Création App CredESI

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "credesi-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style/style.css",
+  "/script/script.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+// Installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activation
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Récupération (offline first)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
